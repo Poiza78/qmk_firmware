3ad58287a39ddd2c961e7096176d29c2869df09a
Poiza <poiza@list.ru>
2020-10-03 13:16:28 +0300

    Layout finished

--- keyboards/handwired/reviung34hw/keymaps/default/keymap.c
+++ keyboards/handwired/reviung34hw/keymaps/default/keymap.c
@@ -23,23 +23,30 @@ enum layer_names {
     SYMB
 };
 
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
-    [BASE] = LAYOUT(
-    KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,		KC_Y,	KC_U,	KC_I,	KC_O,	\
-    S(KC_A), 	C(KC_S),G(KC_D),A(KC_F),KC_G,		A(KC_H),G(KC_J),C(KC_K),S(KC_L),\
-    KC_X,	KC_X,	KC_C,	KC_V,	,KC_B,		KC_N,	KC_M,	KC_P,	KC_SLSH,
-    LT(FUNC, KC_TAB), LT(NUMP, KC_ESC), KC_SPC,		LT(SYMP, KC_ENT)
-
+    [BASE] = LAYOUT_reviung34hw(
+    KC_Q,	  KC_W,		KC_E,	      KC_R,	    KC_T,	KC_Y,	      KC_U,	    KC_I,	  KC_O,		\
+    LSFT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LALT_T(KC_F), KC_G,	LALT_T(KC_H), LGUI_T(KC_J), LCTL_T(KC_K), LSFT_T(KC_L),	\
+    KC_Z,	  KC_X,		KC_C,	      KC_V,	    KC_B,	KC_N,	      KC_M,	    KC_P,	  KC_BSPC,	\
+    			 LT(FUNC, KC_TAB),KC_ESC,LT(NUMP,KC_SPC),	LT(SYMB, KC_ENT)
     ),
-    [FUNC] = LAYOUT(
-
+    [FUNC] = LAYOUT_reviung34hw(
+    KC_F12,	KC_F7,	KC_F8,	KC_F9,	KC_PSCR,	KC_MUTE, KC_VOLU, KC_VOLD, KC_NO,	\
+    KC_F11,	KC_F4,	KC_F5,	KC_F6,	KC_CAPS,	KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT,	\
+    KC_F10,	KC_F1,	KC_F2,	KC_F3,	KC_INS,		KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,	\
+			KC_TAB,	KC_ESC,	KC_SPC,		KC_ENT
     ),
-    [NUMP] = LAYOUT(
-
+    [NUMP] = LAYOUT_reviung34hw(
+    KC_LBRC,	KC_7,	KC_8,	KC_9,	KC_RBRC,	KC_NO,   KC_MINS, KC_NO,   KC_NO,	\
+    KC_LSFT,	KC_4,	KC_5,	KC_6,	KC_EQL,		KC_SLSH, KC_COMM, KC_DOT,  KC_QUOT,	\
+    KC_GRV,	KC_1,	KC_2,	KC_3,	KC_BSLS,	KC_NO,   KC_SCLN, KC_NO,   KC_NO,	\
+			KC_MINS,KC_NO,	KC_NO,		KC_0
     ),
-    [SYMB] = LAYOUT(
-
+    [SYMB] = LAYOUT_reviung34hw(
+    KC_LCBR,	KC_OMPR,KC_OSTR,KC_LPRN,KC_RCBR,	KC_NO,	 KC_NO,   KC_NO,   KC_NO,	\
+    KC_NO,	KC_DLR, KC_PERC,KC_CIRC,KC_PLUS,	KC_LALT, KC_LGUI, KC_LCTL, KC_LSFT,	\
+    KC_TILD, 	KC_EXLM,KC_AT,  KC_HASH,KC_PIPE,	KC_NO,	 KC_NO,   KC_NO,   KC_NO,	\
+			KC_NO,  KC_LPRN,KC_RPRN,	KC_NO
     )
 };
--- keyboards/handwired/reviung34hw/reviung34hw.h
+++ keyboards/handwired/reviung34hw/reviung34hw.h
@@ -20,7 +20,7 @@
 
 #define XXX KC_NO
 
-#define LAYOUT( \
+#define LAYOUT_reviung34hw( \
     K00, K01, K02, K03, K04,     K05, K06, K07, K08,  \
     K10, K11, K12, K13, K14,     K15, K16, K17, K18,  \
     K20, K21, K22, K23, K24,     K25, K26, K27, K28,  \
